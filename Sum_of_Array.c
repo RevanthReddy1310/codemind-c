35251841ed933bb560fcd9442fe4db26c591ad4d
RevanthReddy1310 <147373080+RevanthReddy1310@users.noreply.github.com>
2023-12-17 20:23:33 +0530

    #include<stdio.h> int main() {     int a,num[20],sum=0,i;     scanf("%d",&a);     for(i=1;i<=a;i++){         scanf("%d",&num[20]);         sum+=num[20];     }     printf("%d",sum); }

---
+++ Sum_of_Array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,num[20],sum=0,i;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++){
+        scanf("%d",&num[20]);
+        sum+=num[20];
+    }
+    printf("%d",sum);
+}
