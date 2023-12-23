4b6fe537b3ae2f7ded68a9f7bbbe7672474d2291
RevanthReddy1310 <147373080+RevanthReddy1310@users.noreply.github.com>
2023-12-23 21:22:07 +0530

    #include <stdio.h> int main () {       int n;       scanf("%d", &n);       int arr[n];       int count, min = 0, max = 0;       for (int i = 0; i < n; i++) {           scanf("%d", &arr[i]);       }       for (int i = 0; i < n; i++) {           count=0;           for (int j = 0; j < n; j++) {               if (arr[i] == arr[j]) {                   count++;               }           }           if (count == arr[i]) {               if ( min == 0 || arr[i] < min ) {                   min = arr[i];               }               if ( max == 0 || arr[i] > max ) {                   max = arr[i];               }           }       }       if (min == 0 && max == 0) {           printf("-1");       }       else {           printf("%d %d",min,max);       }   }

---
+++ Maximum_and_Minimum.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main ()
+{
+      int n;
+      scanf("%d", &n);
+      int arr[n];
+      int count, min = 0, max = 0;
+      for (int i = 0; i < n; i++) {
+          scanf("%d", &arr[i]);
+      }
+      for (int i = 0; i < n; i++) {
+          count=0;
+          for (int j = 0; j < n; j++) {
+              if (arr[i] == arr[j]) {
+                  count++;
+              }
+          }
+          if (count == arr[i]) {
+              if ( min == 0 || arr[i] < min ) {
+                  min = arr[i];
+              }
+              if ( max == 0 || arr[i] > max ) {
+                  max = arr[i];
+              }
+          }
+      }
+      if (min == 0 && max == 0) {
+          printf("-1");
+      } 
+      else {
+          printf("%d %d",min,max);
+      }
+  }
