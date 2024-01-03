ad9e0cee707fa0dd66027eb81486f263421e92a7
RevanthReddy1310 <147373080+RevanthReddy1310@users.noreply.github.com>
2024-01-03 10:37:02 +0530

    #include<stdio.h> int main() {     int n,i,j=0;     scanf("%d",&n);     for(i=65;i<=((65+n)-1);i++){         int k=0;         while(k<n){             if(j==k|| k==((n-1)-j)){                 printf("%c",i);             }             else{                 printf(" ");             }             k++;         }         printf(" ");         j++;     } }

---
+++ Pattern_8.4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j=0;
+    scanf("%d",&n);
+    for(i=65;i<=((65+n)-1);i++){
+        int k=0;
+        while(k<n){
+            if(j==k|| k==((n-1)-j)){
+                printf("%c",i);
+            }
+            else{
+                printf(" ");
+            }
+            k++;
+        }
+        printf("
+");
+        j++;
+    }
+}
